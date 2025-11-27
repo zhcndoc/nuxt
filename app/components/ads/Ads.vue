974eaaaf08ba61d019d845699a405d2c9b57c5a7
一纸忘忧 <i@ikxin.com>
2025-11-27 22:52:06 +0800

    chore: add sponsor ads

--- app/components/ads/Ads.vue
+++ app/components/ads/Ads.vue
@@ -2,14 +2,25 @@
 // const { $ads } = useNuxtApp()
 // const route = useRoute()
 // const nuxtApp = useNuxtApp()
-const route = useRoute()
+// const route = useRoute()
 </script>
 
 <template>
   <div class="space-y-3">
-    <AdsMN v-if="route.path.length % 2 === 0" />
+    <!-- <AdsMN v-if="route.path.length % 2 === 0" />
     <AdsNuxtCertificate v-else />
-    <!-- <AdsFallback v-if="$ads.adBlocked.value" />
+    <AdsFallback v-if="$ads.adBlocked.value" />
     <LazyAdsCarbon v-else :key="route.path" /> -->
+    <div class="group relative border border-default rounded-md hover:bg-elevated/50 w-full transition-colors p-2">
+      <ULink
+        to="https://www.rainyun.com/mm_?s=zhcndoc"
+        target="_blank"
+        class="absolute inset-0 z-10"
+        data-umami-event="ads-nuxt-right"
+      />
+      <div class="flex justify-center w-full ">
+        <img src="/assets/ads/rainyun_1.png" class="w-full rounded-sm">
+      </div>
+    </div>
   </div>
 </template>
